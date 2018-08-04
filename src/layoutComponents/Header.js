ef9dfedf7ca7b6b25ac6716fd46166a180f9fb40
nathanolmstead <nathan@a2unedsolutions.com>
2018-08-04 07:10:38 -0400

    a2uned.io site migration updates

--- src/layoutComponents/Header.js
+++ src/layoutComponents/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from '../images/a2uned_solutions_logo.svg';
+import logo from '../images/a2uned_solutions_io_logo.png';
 
 export default function Header() {
   return (
     <header className="header">
-      <img src={logo} className="logo" alt="A2uned Solutions" />
-      <a href="https://blog.a2unedsolutions.com" className="blog-link">Blog</a>
+      <img src={logo} className="logo" alt="A2uned.io" />
+      <nav>
+        <ul>
+          <li>
+            <a href="https://a2unedsolutions.com">a2unedsolutions.com</a>
+          </li>
+          <li>
+            <a href="https://blog.a2unedsolutions.com">blog</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
