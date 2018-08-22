5a50d6fafec46cf59fb0fba775a0f45047dc3808
nathanolmstead <nathan@a2unedsolutions.com>
2018-08-22 15:36:04 -0400

    fix header link attributes

--- src/layoutComponents/Header.js
+++ src/layoutComponents/Header.js
@@ -8,10 +8,10 @@ export default function Header() {
       <nav>
         <ul>
           <li>
-            <a href="https://www.a2unedsolutions.com" ref="follow">a2unedsolutions.com</a>
+            <a href="https://www.a2unedsolutions.com">a2unedsolutions.com</a>
           </li>
           <li>
-            <a href="https://blog.a2unedsolutions.com" ref="follow">blog</a>
+            <a href="https://blog.a2unedsolutions.com">blog</a>
           </li>
         </ul>
       </nav>
